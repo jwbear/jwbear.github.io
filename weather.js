9cdf0d2f5562c3ced4ad193841ebd2709232edd6
Jay White Bear <jwhiteb@us.ibm.com>
2017-04-22 23:52:48 -0700

    Update weather.js

--- weather.js
+++ weather.js
@@ -11,9 +11,8 @@
     ext.get_temp = function(location, callback) {
         // Make an AJAX call to the Open Weather Maps API
         $.ajax({
-              url: 'http://api.openweathermap.org/data/2.5/weather?q='+location+'&units=imperial',
+              url: 'http://api.openweathermap.org/data/2.5/weather?q='+location+'&units=imperial&APPID=0839f1e7c6c46680879e8b06e86d8714',
               dataType: 'jsonp',
-              APPID: "0839f1e7c6c46680879e8b06e86d8714"
               success: function( weather_data ) {
                   // Got the data - parse it and return the temperature
                   temperature = weather_data['main']['temp'];
