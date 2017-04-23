39c2839ef68678aba6fbddc3080aad65b95daa72
Jay White Bear <jwhiteb@us.ibm.com>
2017-04-23 01:42:03 -0700

    Update weather_debug.js

--- weather_debug.js
+++ weather_debug.js
@@ -1,7 +1,7 @@
 alert("start");
 
         // Make an AJAX call to the Open Weather Maps API
-function(callback) {
+get_temp = function(callback) {
         $.ajax({
               url: 'http://api.openweathermap.org/data/2.5/weather?q='+'Boston,MA'+'&units=imperial&APPID=0839f1e7c6c46680879e8b06e86d8714',
               dataType: 'jsonp',
@@ -12,4 +12,4 @@ function(callback) {
               } 
         });
 };
-     alert("end");
+     alert(get_temp);
