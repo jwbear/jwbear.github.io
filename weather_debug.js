ac894c093951cf37ecea04374d6c42516cdb1f76
Jay White Bear <jwhiteb@us.ibm.com>
2017-04-23 00:32:24 -0700

    Add files via upload

---
+++ weather_debug.js
@@ -0,0 +1,14 @@
+alert("start");
+
+        // Make an AJAX call to the Open Weather Maps API
+        $.ajax({
+              url: 'http://api.openweathermap.org/data/2.5/weather?q='+'Boston,MA'+'&units=imperial&APPID=0839f1e7c6c46680879e8b06e86d8714',
+              dataType: 'jsonp',
+              success: function( weather_data ) {
+                  // Got the data - parse it and return the temperature
+                  temperature = "meep"; //weather_data['main']['temp'];
+                  callback(temperature);
+              }
+        });
+    
+alert(temperature);
