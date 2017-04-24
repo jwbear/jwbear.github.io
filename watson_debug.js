f9c634c7add3e0c739d20006903844634360934e
Jay White Bear <jwhiteb@us.ibm.com>
2017-04-24 01:40:39 -0700

    Update watson_debug.js

--- watson_debug.js
+++ watson_debug.js
@@ -3,7 +3,7 @@ alert("start");
 $( document ).ready(function (callback) {
        //AJAX Call to Watson API
     	$.ajax({
-    		url: 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19&text=A%20word%20is%20dead%20when%20it%20is%20said,%20some%20say.%20Emily%20Dickinson',
+    		url: 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19&text=helloworld',
               username:'f2a9cfc8-3322-41f3-b188-95a90ecf7e9e', 
               password:'2CLTsQkdjCQa',
               dataType: 'jsonp',
