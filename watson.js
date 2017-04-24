a5c2dd89b9df7584b698f1817b526f1e7cf04470
Jay White Bear <jwhiteb@us.ibm.com>
2017-04-23 18:17:11 -0700

    Update watson.js

--- watson.js
+++ watson.js
@@ -25,8 +25,8 @@
     	//AJAX Call to Watson API
     	$.ajax({
     		  url: 'https://gateway.watsonplatform.net/tone-analyzer/api',
-              //username: 'f2a9cfc8-3322-41f3-b188-95a90ecf7e9e',
-  			  //password: '2CLTsQkdjCQa',
+              username: 'f2a9cfc8-3322-41f3-b188-95a90ecf7e9e',
+  			  password: '2CLTsQkdjCQa',
               dataType: 'jsonp',
               success: function( tone_data ) {
                   // Got the data - parse it and return the temperature
