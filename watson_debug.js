f3799eeba35f756ff81a4913845af1ea0e587c2e
Jay White Bear <jwhiteb@us.ibm.com>
2017-04-24 11:12:50 -0700

    Update watson_debug.js

--- watson_debug.js
+++ watson_debug.js
@@ -3,15 +3,15 @@
 $( document ).ready(function (callback) {
        //AJAX Call to Watson API
     	$.ajax({
-    		//url: 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19&text=helloworld',
-              //username:'f2a9cfc8-3322-41f3-b188-95a90ecf7e9e', 
-              //password:'2CLTsQkdjCQa',
+    		url: 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19&text=helloworld',
+              username:'f2a9cfc8-3322-41f3-b188-95a90ecf7e9e', 
+              password:'2CLTsQkdjCQa',
               dataType: 'jsonp',
               success: function( tone_data ) {
                   // Got the data - parse
-                  //tone = tone_data[document_tone][tone_categories][0][tones][0][tone_name];
+                  tone = tone_data[document_tone][tone_categories][0][tones][0][tone_name];
                   //alert(tone);
-                  //callback(tone);
+                  callback(tone);
               }
         });
 });
