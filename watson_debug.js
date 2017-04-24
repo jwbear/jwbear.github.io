eb16668fd37651173a5630f0366a9b3d2d7c2560
Jay White Bear <jwhiteb@us.ibm.com>
2017-04-23 19:09:38 -0700

    Update watson_debug.js

--- watson_debug.js
+++ watson_debug.js
@@ -6,7 +6,7 @@ $( document ).ready(function (callback) {
     		url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2017-02-27',
               beforeSend: function(xhr) { 
                      xhr.setRequestHeader("Authorization", "Basic " + btoa("f2a9cfc8-3322-41f3-b188-95a90ecf7e9e:2CLTsQkdjCQa")); 
-              }
+              },
               dataType: 'jsonp',
               success: function( tone_data ) {
                   // Got the data - parse it and return the temperature
