36d3cc74945333582f5c007e9231f2237d522c08
Jay White Bear <jwhiteb@us.ibm.com>
2017-04-24 01:30:46 -0700

    Update watson.js

--- watson.js
+++ watson.js
@@ -24,13 +24,13 @@
     ext.get_tone = function(sent, callback){
     	 //AJAX Call to Watson API
     	$.ajax({
-    		  url: 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19&text=A%20word%20is%20dead%20when%20it%20is%20said,%20some%20say.%20Emily%20Dickinson',
+    		url: 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19&text=A%20word%20is%20dead%20when%20it%20is%20said,%20some%20say.%20Emily%20Dickinson',
               username:'f2a9cfc8-3322-41f3-b188-95a90ecf7e9e', 
               password:'2CLTsQkdjCQa',
               dataType: 'jsonp',
               success: function( tone_data ) {
-                  // Got the data - parse it and return the temperature
-                  tone = tone_data; //tone_data[document][emotion][joy];
+                  // Got the data - parse
+                  tone = tone_data[document_tone][tone_categories][0][tones][0][tone_name];
                   alert(tone);
                   callback(tone);
               }
