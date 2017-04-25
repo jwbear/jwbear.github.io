e88c4ad96006bb667e79cb75fc4d06220c7f06d4
Jay White Bear <jwhiteb@us.ibm.com>
2017-04-24 23:41:33 -0700

    Update watson_debug.js

--- watson_debug.js
+++ watson_debug.js
@@ -6,7 +6,8 @@ $( document ).ready(function (callback) {
     		url: 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19&text=helloworld&callback=?',
               username:'f2a9cfc8-3322-41f3-b188-95a90ecf7e9e', 
               password:'2CLTsQkdjCQa',
-              //dataType: 'json',
+              dataType: 'jsonp',
+              jsonpCallback: 'callback',
               success: function( tone_data ) {
                   // Got the data - parse
                   tone = tone_data[document_tone][tone_categories][0][tones][0][tone_name];
