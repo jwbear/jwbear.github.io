f9ad7a0129b20db1b2e37593d8b13a1a67176b37
Jay White Bear <jwhiteb@us.ibm.com>
2017-04-24 23:50:39 -0700

    Update watson_debug.js

--- watson_debug.js
+++ watson_debug.js
@@ -3,7 +3,7 @@
 $( document ).ready(function (callback) {
        //AJAX Call to Watson API
     	$.ajax({
-    		url: 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19&text=helloworld&callback=?',
+    		url: 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19&text=helloworld?callback=?',
               username:'f2a9cfc8-3322-41f3-b188-95a90ecf7e9e', 
               password:'2CLTsQkdjCQa',
               dataType: 'json jsonp',
