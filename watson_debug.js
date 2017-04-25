7481c512bb5b5bafeb0bcc681d23a689d1fb2474
Jay White Bear <jwhiteb@us.ibm.com>
2017-04-25 00:04:11 -0700

    Update watson_debug.js

--- watson_debug.js
+++ watson_debug.js
@@ -8,11 +8,11 @@ $( document ).ready(function (callback) {
               password:'2CLTsQkdjCQa',
               dataType: 'jsonp',
               jsonpCallback: 'callback',
-              success: function( tone_data ) {
+              /*success: function( tone_data ) {
                   // Got the data - parse
                   console.log("hey");
                   tone = tone_data[document_tone][tone_categories][0][tones][0][tone_name];
-                  callback(tone);
+                  callback(tone);*/
               }
         }); 
 });
